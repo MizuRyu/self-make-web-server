23fc02b1174879c41b26319d1f34707eeda90421
MizuRyu <02rlmdev@gmail.com>
2025-03-29 17:05:04 +0900

    fix: tcp serv -> web serv(response header付加)

--- webserver.py
+++ webserver.py
@@ -1,8 +1,8 @@
 import socket
-
-class TCPServer:
+from datetime import datetime
+class WebServer:
     """
-    TCP通信を行うサーバーを表すクラス
+    Webサーバを表すクラス
     """
     def serve(self):
         """
@@ -30,6 +30,12 @@ class TCPServer:
             # リクエストデータをファイルに書き出し
             with open("server_recv.txt", "wb") as f:
                 f.write(req_data)
+
+            response_body = self.get_body()
+            response_header = self.get_header(response_body)
+            response = f"{response_header}\r\n{response_body}".encode()
+
+            client_socket.send(response)
             
             # コネクションを終了する
             client_socket.close()
@@ -37,6 +43,26 @@ class TCPServer:
         finally:
             print("=== Closing TCP Server ===")
 
+    def get_header(self, response_body):
+        """
+        HTTPレスポンスヘッダを設定
+        """
+        header = ""
+        header = "HTTP/1.1 200 OK\r\n"
+        header += f"Date: {datetime.now().strftime('%a, %d %b %Y %H:%M:%S GMT')}\r\n"
+        header += "Server: DemoServer\r\n"
+        header += f"Content-Length: {len(response_body.encode())}\r\n"
+        header += "Connecion: close\r\n"
+        header += "Content-Type: text/html\r\n"
+        return header
+    
+    def get_body(self):
+        """
+        HTTPレスポンスボディを設定
+        """
+        body = "<html><body><h1>Hello, World!</h1></body></html>"
+        return body
+
 if __name__ == "__main__":
-    server = TCPServer()
+    server = WebServer()
     server.serve()
